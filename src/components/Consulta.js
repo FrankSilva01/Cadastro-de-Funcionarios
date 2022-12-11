77a58c17d5fdda2d8017685177baa84c3fdbb0d4
Frank Silva <101152046+FrankSilva01@users.noreply.github.com>
2022-12-11 01:01:05 -0300

    Update Consulta.js

--- src/components/Consulta.js
+++ src/components/Consulta.js
@@ -7,7 +7,7 @@ import {useState} from "react"
 export default function Consulta(props) {
 
   // usada para guardar os valores digitados na barra de pesquisa
-  const [nomes, setNomes] = useState()
+  const [nomes, setNomes] = useState('')
  
 // Os valores recebidos por props do array usuario no componente App esta sendo colocado nas variáveis abaixo
   let nome = props.usuario[0]
@@ -18,17 +18,18 @@ export default function Consulta(props) {
   return (
     <div>
          <NavBar />
-         <h1>Consulta</h1>
+             <h1>Consulta</h1>
+         <p>Digite abaixo o nome do funcionário</p>
        
-         <br />
+        <br />
          {/* Função onChange jogando o valor digitado na variável nomes */}
              <input id="pesquisar" name="pesquisar" placeholder="Digite o nome do colaborador"  onChange={(e)=> setNomes(e.target.value)} width="350px" />{" "}
                 
                 {/* Comparação do valor digitado com o salvo no componente CadastroFuncionário que esta armanezanado na variável, 
                 se for dígitado na pesquisa o nome igual ao do cadastro retornar os dados caso contrário retorna null */}
-                <ul key={nomes}>
+                <ul key={nomes} className={style.listStyle}>
                  <li>
-                     <h4 >{nome === nomes ? 'Nome do funcionário: ' + nome : ''}</h4>
+                     <h4 >{nome === nomes ? 'Nome do funcionário: ' + nome : ' '}</h4>
                       </li> 
                          <li>
                            <p>{nome === nomes ? 'Cargo: ' + cargo : ''} </p>
@@ -40,7 +41,7 @@ export default function Consulta(props) {
                      <p>{nome === nomes ? 'Salário: ' + salario : ''} </p>
                   </li>
                 <li>
-                    <NavLink to="/detalhes"  className={nome === nomes ? style.detalhesLink : null}>  Detalhes</NavLink>
+                    <NavLink to="/detalhes" className={nome === nomes ? style.detalhesLink : ''} >  Detalhes</NavLink>
                    </li>
               </ul>
     </div>
